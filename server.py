c0898bb4d028183b7a577eea620c4c50b6c5e101
P17 <P17,@example.com>
2024-09-24 16:05:57 -0700

    "Initializing repo history"

---
+++ server.py
@@ -0,0 +1,76 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from urllib.parse import parse_qs, urlparse
+import json
+import pandas as pd
+from datetime import datetime
+import uuid
+import os
+from typing import Callable, Any
+from wsgiref.simple_server import make_server
+
+nltk.download('vader_lexicon', quiet=True)
+nltk.download('punkt', quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
+nltk.download('stopwords', quiet=True)
+
+adj_noun_pairs_count = {}
+sia = SentimentIntensityAnalyzer()
+stop_words = set(stopwords.words('english'))
+
+reviews = pd.read_csv('data/reviews.csv').to_dict('records')
+
+class ReviewAnalyzerServer:
+    def __init__(self) -> None:
+        # This method is a placeholder for future initialization logic
+        pass
+
+    def analyze_sentiment(self, review_body):
+        sentiment_scores = sia.polarity_scores(review_body)
+        return sentiment_scores
+
+    def __call__(self, environ: dict[str, Any], start_response: Callable[..., Any]) -> bytes:
+        """
+        The environ parameter is a dictionary containing some useful
+        HTTP request information such as: REQUEST_METHOD, CONTENT_LENGTH, QUERY_STRING,
+        PATH_INFO, CONTENT_TYPE, etc.
+        """
+
+        if environ["REQUEST_METHOD"] == "GET":
+            # Create the response body from the reviews and convert to a JSON byte string
+            response_body = json.dumps(reviews, indent=2).encode("utf-8")
+            
+            # Write your code here
+            query = parse_qs(environ.get('QUERY_STRING', ''))
+            location = query.get('Location', [None])[0]
+            timestamp = query.get('Timestamp', [None])[0]
+            # Filter reviews based on the parameters
+            # Assuming `reviews` is a list of dictionaries and you have a way to filter it
+            if location:
+                filtered_reviews = [review for review in reviews if review['Location'] == location]
+                print(location)
+            if timestamp:
+                filtered_reviews = [review for review in reviews if review['Timestamp'] == timestamp]
+                print(timestamp)
+            
+
+            # Set the appropriate response headers
+            start_response("200 OK", [
+            ("Content-Type", "application/json"),
+            ("Content-Length", str(len(response_body)))
+             ])
+            
+            return [response_body]
+
+
+        if environ["REQUEST_METHOD"] == "POST":
+            # Write your code here
+            return
+
+if __name__ == "__main__":
+    app = ReviewAnalyzerServer()
+    port = os.environ.get('PORT', 8000)
+    with make_server("", port, app) as httpd:
+        print(f"Listening on port {port}...")
+        httpd.serve_forever()
